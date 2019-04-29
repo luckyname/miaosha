1181fb684c6c06199c4b7d1dcf1423ac05c4b828
Shundong Wu <shundong106@icloud.com>
2019-04-29 10:43:18 +0800

    注册模块 opt短信v1.0

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -11,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 /**
  * @author ：shundong.wu
  * @date ：Created in 2019/4/28 15:40
@@ -21,6 +24,25 @@ import org.springframework.web.bind.annotation.*;
 public class UserController extends BaseController {
     @Autowired
     private UserServiceImpl userService;
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+    //用户获取otp短信接口
+    @RequestMapping("/getopt")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "iphone") String iphone) {
+    // 1.需要按照一定的规则生成OTP的验证码
+        Random random = new Random();
+        int radomInt = random.nextInt(99999);
+        radomInt+=10000;
+        String optCode = String.valueOf(radomInt);
+    // 2.将OTP验证码同对应的用户手机号关联（redis最佳  暂时使用HttpSession)
+        httpServletRequest.getSession().setAttribute(iphone,optCode);
+
+    // 3.将OTP验证码通过短信的通道发送给用户(省略)
+        System.out.println("telphone="+iphone +"    &otpCode="+optCode);
+        return CommonReturnType.create(null);
+    }
+
 
     @RequestMapping("/get")
     public @ResponseBody
